73ec253009d20d6a64bcf98bf1df941e41e2b5d5
Alan Orth <alan.orth@gmail.com>
2013-07-14 22:16:45 +0300

    pelican.conf.py: Update blogroll with real links

--- pelican.conf.py
+++ pelican.conf.py
@@ -16,10 +16,9 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Mjanja Tech', 'http://mjanja.co.ke'),
+          ('Moshe Njema', 'http://nj3ma.wordpress.com/'),
+          ('David Karibe', 'http://karibe.co.ke/'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
