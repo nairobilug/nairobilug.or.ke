b1cdab9da55247b82df3ece8aa3345dc45e5d8a3
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-07-14 16:42:32 -0300

    Update documentation

--- theme/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	// Auto-handle direct jpeg links
-	$('a[href$="jpg"]').each(function() {
-        $(this).colorbox({
-            maxWidth: '100%',
-            maxHeight: '100%'
-        });
-	});
-});
