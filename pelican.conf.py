e3b09f47f27866c3474fddc9cd6f503532e6b631
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-09-13 04:44:04 +0300

    Add twitter address

--- pelican.conf.py
+++ pelican.conf.py
@@ -30,6 +30,7 @@ SOCIAL = (
 
 # Crowsfoot settings
 GITHUB_ADDRESS = 'https://github.com/nairobilug'
+TWITTER_ADDRESS = 'https://twitter.com/nairobilug'
 PROFILE_IMAGE_URL = 'https://1.gravatar.com/avatar/8e936023d8f7a911a528c78957edbc93?s=200'
 SITESUBTITLE = 'Nairobi GNU/Linux Users Group'
 
