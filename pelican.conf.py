8aafe66f2218f564a4915bdc4e6d26c40e022e17
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-09-15 14:04:27 +0300

    Format config

--- pelican.conf.py
+++ pelican.conf.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+
 AUTHOR = 'Multiple'
 SITENAME = 'Nairobi LUG'
 SITEURL = 'http://nairobilug.or.ke'
 
+DEFAULT_LANG = 'en'
 TIMEZONE = 'Africa/Nairobi'
 
-DEFAULT_LANG = 'en'
-
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
@@ -39,14 +39,14 @@ THEME = 'crowsfoot'
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
-ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{slug}-{lang}.html'
-PAGE_URL = '{date:%Y}/{date:%m}/pages/{slug}.html'
-PAGE_LANG_URL = '{date:%Y}/{date:%m}/pages/{slug}-{lang}.html'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
 ARTICLE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/{slug}-{lang}.html'
-PAGE_SAVE_AS = '{date:%Y}/{date:%m}/pages/{slug}.html'
+ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{slug}-{lang}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
 PAGE_LANG_SAVE_AS = '{date:%Y}/{date:%m}/pages/{slug}-{lang}.html'
+PAGE_LANG_URL = '{date:%Y}/{date:%m}/pages/{slug}-{lang}.html'
+PAGE_SAVE_AS = '{date:%Y}/{date:%m}/pages/{slug}.html'
+PAGE_URL = '{date:%Y}/{date:%m}/pages/{slug}.html'
 
 # Copy CNAME to output root
 FILES_TO_COPY = (
