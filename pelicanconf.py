a5b9938dc3746c661ae311ac4ff28de978c7477f
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2014-06-21 14:55:28 +0300

    Add theme META_DESCRIPTION option

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,6 +40,10 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
 PROFILE_IMAGE = '/images/profile.png'
 SHOW_ARTICLE_AUTHOR = True
 SITE_SUBTEXT = 'Nairobi GNU/Linux Users Group'
+META_DESCRIPTION = '''Nairobi GNU/Linux Users Group is a not-for-profit
+                   community serving the greater Nairobi area. We are a
+                   collection of people dedicated to GNU/Linux, Free Software,
+                   Open Source, and other related topics.'''
 
 # LICENSE_URL = ''
 # LICENSE_NAME = ''
